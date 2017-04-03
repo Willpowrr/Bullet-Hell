05819e957b316a72c84041374f78b2742fb34b06
Willpowrr <willpowrr@gmail.com>
2017-04-02 23:01:10 -0700

    Enemies spawn at random Y positions

--- Final/Assets/Scripts/Enemies/BH_Enemy.cs
+++ Final/Assets/Scripts/Enemies/BH_Enemy.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 namespace BulletHell {
     public class BH_Enemy : MonoBehaviour {
+        
+        public string enemyID;
 
         [SerializeField]
         protected BezierCurve movementCurve;
 
-        // Use this for initialization
-        void Start() {
+        [SerializeField]
+        protected float lifeTime;
 
+        public BH_MovementController movementController { get; protected set; }
+
+        public bool active { get; set; }
+        public bool livedItsLife { get { return (Time.fixedTime - birthTime) > lifeTime; } }
+
+        protected float birthTime = 0.0f;
+
+        private void Awake() {
+            movementController = GetComponent<BH_MovementController>();
+            active = false;
+        }
+        
+        void Start() {
+        }
+        
+        void Update() {
         }
 
-        // Update is called once per frame
-        void Update() {
-
+        public void Spawn() {
+            birthTime = Time.fixedTime;
+            active = true;
         }
     }
 }
