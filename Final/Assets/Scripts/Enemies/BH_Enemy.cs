2a2dfafa148c1b9537919b016624642c8d73e1c7
Willpowrr <willpowrr@gmail.com>
2017-04-02 20:36:13 -0700

    Enemy / Curve stuff

---
+++ Final/Assets/Scripts/Enemies/BH_Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BulletHell {
+    public class BH_Enemy : MonoBehaviour {
+
+        [SerializeField]
+        protected BezierCurve movementCurve;
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
---
+++ Final/Assets/Scripts/Enemies/Dumbass/BH_DumbassEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BulletHell {
+    public class BH_DumbassEnemy : BH_Enemy {
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
