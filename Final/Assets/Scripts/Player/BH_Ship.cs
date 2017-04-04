b0006554cf4722167655889119fdc9c76eec0d69
Willpowrr <willpowrr@gmail.com>
2017-04-03 21:57:55 -0700

    Hooked up particles

--- Final/Assets/Scripts/Player/BH_Ship.cs
+++ Final/Assets/Scripts/Player/BH_Ship.cs
@@ -6,11 +6,63 @@ namespace BulletHell {
 
     public class BH_Ship : MonoBehaviour {
 
+        public enum HorizontalMovementState : int {
+            Idle = 0,
+            Forward,
+            Backward
+        }
+
         public BH_Player player { get; protected set; }
         public BH_MovementController movementController { get; protected set; }
         public BH_InputController inputController { get; protected set; }
 
+        [SerializeField]
+        protected List<ParticleSystem> idleParticles = new List<ParticleSystem>();
+
+        [SerializeField]
+        protected List<ParticleSystem> forwardParticles = new List<ParticleSystem>();
+
+        [SerializeField]
+        protected List<ParticleSystem> backwardParticles = new List<ParticleSystem>();
+
         public float moveSpeed;
+
+        protected HorizontalMovementState _horizontalMovementState = HorizontalMovementState.Idle;
+        public HorizontalMovementState horizontalMovementState {
+            get { return _horizontalMovementState; }
+            set {
+                if (_horizontalMovementState == value) {
+                    return;
+                }
+                switch (_horizontalMovementState) {
+                    case HorizontalMovementState.Idle:
+                        SetIdleParticlesEnabled(false);
+                        break;
+
+                    case HorizontalMovementState.Forward:
+                        SetForwardParticlesEnabled(false);
+                        break;
+
+                    case HorizontalMovementState.Backward:
+                        SetBackwardParticlesEnabled(false);
+                        break;
+                }
+                _horizontalMovementState = value;
+                switch (_horizontalMovementState) {
+                    case HorizontalMovementState.Idle:
+                        SetIdleParticlesEnabled(true);
+                        break;
+
+                    case HorizontalMovementState.Forward:
+                        SetForwardParticlesEnabled(true);
+                        break;
+
+                    case HorizontalMovementState.Backward:
+                        SetBackwardParticlesEnabled(true);
+                        break;
+                }
+            }
+        }
 
         public void Awake() {
 
@@ -25,6 +77,8 @@ namespace BulletHell {
         // Use this for initialization
         void Start() {
 
+            DisableAllParticles();
+            SetIdleParticlesEnabled(true);
         }
 
         // Update is called once per frame
@@ -33,9 +87,14 @@ namespace BulletHell {
             Vector3 newVelocity = Vector3.zero;
             if (inputController.right) {
                 newVelocity.x += moveSpeed;
+                horizontalMovementState = HorizontalMovementState.Forward;
             }
             else if (inputController.left) {
                 newVelocity.x -= moveSpeed;
+                horizontalMovementState = HorizontalMovementState.Backward;
+            }
+            else {
+                horizontalMovementState = HorizontalMovementState.Idle;
             }
 
             if (inputController.up) {
@@ -46,6 +105,31 @@ namespace BulletHell {
             }
 
             movementController.SetVelocity(newVelocity);
+        }
+
+        protected void SetParticleSystemsEnabled(List<ParticleSystem> p_list, bool p_enabled) {
+            foreach (ParticleSystem particleSystem in p_list) {
+                ParticleSystem.EmissionModule emissionModule = particleSystem.emission;
+                emissionModule.enabled = p_enabled;
+            }
+        }
+
+        protected void DisableAllParticles() {
+            SetIdleParticlesEnabled(false);
+            SetForwardParticlesEnabled(false);
+            SetBackwardParticlesEnabled(false);
+        }
+
+        public void SetIdleParticlesEnabled(bool p_enabled) {
+            SetParticleSystemsEnabled(idleParticles, p_enabled);
+        }
+
+        public void SetForwardParticlesEnabled(bool p_enabled) {
+            SetParticleSystemsEnabled(forwardParticles, p_enabled);
+        }
+
+        public void SetBackwardParticlesEnabled(bool p_enabled) {
+            SetParticleSystemsEnabled(backwardParticles, p_enabled);
         }
     }
 }
