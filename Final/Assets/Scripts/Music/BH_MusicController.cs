33b8c089f9c529c68fcedae343fc0065773f39a7
Willpowrr <willpowrr@gmail.com>
2017-04-05 22:46:32 -0700

    Music

---
+++ Final/Assets/Scripts/Music/BH_MusicController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BulletHell {
+    public class BH_MusicController : MonoBehaviour {
+
+        public AudioSource audioSource { get; protected set; }
+
+        [SerializeField]
+        protected AudioClip musicClip;
+
+        private void Awake() {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        void Start() {
+            audioSource.clip = musicClip;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
