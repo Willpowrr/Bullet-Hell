0f512cf79cfee2bc6db26ba332df518da5a0127b
Willpowrr <willpowrr@gmail.com>
2017-04-09 15:33:32 -0700

    Fixxxxxed

---
+++ Final/Assets/Scripts/Bullet/BH_Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BulletHell;
+
+namespace BulletHell {
+
+    public class BH_Bullet : MonoBehaviour {
+        
+        public Rigidbody rigidBody { get; protected set; }
+        public BH_BulletController bulletController { get; set; }
+
+        public void Awake() {
+            rigidBody = GetComponent<Rigidbody>();
+        }
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        private void OnTriggerEnter(Collider other) {
+            Kill();
+        }
+
+        protected void Kill() {
+            bulletController.ReturnBullet(this);
+            bulletController.CreateExplosionParticles(transform.position);
+        }
+
+        protected void OnFastDestroy() {
+
+        }
+    }
+}
